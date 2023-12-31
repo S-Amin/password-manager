ba25e56c28d499cbf115bce252a83ba0351572e3
Saeed Amin <s4ied.amin@gmail.com>
2023-12-31 23:50:17 +0100

    fix bundling and extension

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,17 +1,17 @@
 import Fastify from 'fastify'
 import fastifyStatic from '@fastify/static'
 import path from 'node:path'
+import fs from 'node:fs'
 
 const fastify = Fastify({
     logger: true,
 })
 
-const root = path.join(__dirname, '../../public') //import.meta.url;
+const root = path.join(__dirname, '../../public')
 
 fastify.register(fastifyStatic, {
     root,
-    prefix: '/public/', // optional: default '/'
-    // constraints: { host: "example.com" }, // optional: default {}
+    prefix: '/public/', // https://github.com/fastify/fastify-static
 })
 
 // Declare a route
@@ -20,7 +20,8 @@ fastify.get('/', (_request, reply) => {
 })
 
 fastify.get('/password-manager', (_request, reply) => {
-    reply.sendFile('index.html')
+    const file = fs.readFileSync('./index.html')
+    reply.type('text/html').send(file)
 })
 
 // Run the server!
