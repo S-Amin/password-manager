7b7a030c8ed94549e56880aa5c724b5c797bc93b
Saeed Amin <s4ied.amin@gmail.com>
2024-01-01 18:09:05 +0100

    minor refactor

--- src/shared/popup.ts
+++ src/shared/popup.ts
@@ -3,29 +3,31 @@ import { UIMode } from './types.ts'
 import { FormStore } from './formStore.ts'
 import { PassGeneratorService } from './passGeneratorService.ts'
 
-const uiMode: UIMode = getEnv()
+onOpen()
 
-new FormStore(
-    {
-        loginInput: 'loginId',
-        domainInput: 'domain',
-        masterPassInput: 'masterPass',
-        passLengthInput: 'passLength',
-        variantInput: 'variant',
-        generateBtn: 'generateBtn',
-        resetBtn: 'resetFrom',
-        copyBtn: 'copy',
-        saveBtn: 'saveConfig',
-        toggleMasterPassVisibility: 'showHidePass',
-        passPreview: 'passPreview',
-        passConfigList: 'passConfigList',
-    },
-    new PassGeneratorService(),
-    new PassConfigStorage(uiMode)
-)
-
-registerInfoView()
-addUIMode()
+function onOpen() {
+    const uiMode: UIMode = getEnv()
+    addUIMode(uiMode)
+    new FormStore(
+        {
+            loginInput: 'loginId',
+            domainInput: 'domain',
+            masterPassInput: 'masterPass',
+            passLengthInput: 'passLength',
+            variantInput: 'variant',
+            generateBtn: 'generateBtn',
+            resetBtn: 'resetFrom',
+            copyBtn: 'copy',
+            saveBtn: 'saveConfig',
+            toggleMasterPassVisibility: 'showHidePass',
+            passPreview: 'passPreview',
+            passConfigList: 'passConfigList',
+        },
+        new PassGeneratorService(),
+        new PassConfigStorage(uiMode)
+    )
+    registerInfoView()
+}
 
 function registerInfoView() {
     document.querySelectorAll('.floatingIcon').forEach((elm) => {
@@ -43,26 +45,7 @@ function registerInfoView() {
     })
 }
 
-// function onOpen() {
-//     const configStorage = new PassConfigStorage(uiMode)
-
-//     let extensionService: ExtensionService
-//     if (chrome.tabs) {
-//         extensionService = new ExtensionService(domainInput, loginIdInput)
-//     }
-//     try {
-//         addUIMode()
-//         registerHandlers(configStorage)
-//         addInputRules([domainInput, loginIdInput])
-
-//         // the subscription will be executed at the end of the function because of async load
-// configStorage.subscribe('LIST_LOADED', loadAllPassConfig)
-//     } catch (e) {
-//         console.error(e)
-//     }
-// }
-
-function addUIMode() {
+function addUIMode(uiMode) {
     const body = document.getElementsByTagName('body')[0]
     body.classList.add(uiMode)
 }
